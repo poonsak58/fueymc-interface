a4b3688c78e1a35ab0a87ab55c091521f537819b
Poonsak Aphidetmongkhon <poonsak_lds@hotmail.com>
2021-12-26 13:07:15 +0700

    add working word

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          FUEYMC NAJA
+          FUEYMC NAJA Working
         </a>
       </header>
     </div>
